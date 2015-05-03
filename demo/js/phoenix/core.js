0c7ee160d323578f7d76ccbedb5c76113c770c8f
Clechay <luke.clechay@gmail.com>
2015-05-03 13:07:44 +0200

    rewrite modules loading and dependencies control system

--- demo/js/phoenix/core.js
+++ demo/js/phoenix/core.js
@@ -7,33 +7,101 @@
 var phoenix = {};
 
 phoenix.data = new Object();
+phoenix.core = new Object();
+phoenix.data.core = new Object();
 
-phoenix.data.compatibility=false;
+phoenix.data.core.compatibility=false;
 
-phoenix.data.modulesList={
-	variables:false,
-	ui:false,
-	ajax:false,
-	events:false
+phoenix.data.core.modulesList={};
+
+phoenix.data.core.modulesList.bowser=true; // this lib is needed to check compatibility
+
+
+phoenix.core.log = function (id,data) {
+	var out = {text:"placeholder",type:"error"};
+	switch(id) {
+	    case 'start_module_loading':
+	        out.text = "[phoenix][core] module "+data+" initializing...";
+	        out.type = "log";
+	        break;
+	    case 'finish_module_loading':
+	        out.text = "[phoenix][core] module "+data+" initialized successfuly";
+	        out.type = "log";
+	        break;
+	    case 'could_not_load_module':
+	        out.text = "[phoenix][core] module "+data.id+" can not be loaded ";
+	        switch(data.reason){
+	        	case 'deps':
+	        		out.text+="because of misssing dependencies";
+	        		break;
+	        	case 'comp':
+	        		out.text+="because phoenix can not start";
+	        		break;
+	        	default:
+	        		out.text+="because of unknown reason";
+	        		break;
+	    	}
+	        out.type = "error";
+	        break;
+	    default:
+	        out.text = "[phoenix][core]undefined log command";
+	        out.type = "error";
+	}
+	switch(out.type){
+		case 'log':
+	        console.log(out.text);
+	        break;
+		case 'error':
+	        console.error(out.text);
+	        break;
+		case 'info':
+	        console.info(out.text);
+	        break;
+		case 'warn':
+	        console.warn(out.text);
+	        break;
+	    default:
+	        console.err("[phoenix][core][log] fatall error - unrecognized_command_type");
+	        console.err("[phoenix][core][log] text:"+out.text);
+	        console.err("[phoenix][core][log] type:"+out.type);
+	}
+	return 1;
+};
+phoenix.core.addModule = function (id) {
+	phoenix.data.core.modulesList[id]=true;
+	return 1;
+};
+phoenix.core.checkDeps = function (dependencies) {
+	for (var i = 0; i < dependencies.length; i++) {
+		if (typeof phoenix.data.core.modulesList[dependencies[i]] === "undefined") {
+			return 0;
+		};
+	};
+	return 1;
+};
+phoenix.core.checkComp = function () {
+	return phoenix.data.core.compatibility;
 };
 
-phoenix.compatibilityCheck = function () {
-	if (bowser.msie && bowser.version <= 8) {
-	  	return false;
-	}
-	else{
-		return true;
-	}
-};
-
-phoenix.init = function () {
-	if (this.compatibilityCheck()) {
-		console.log("[phoenix-init] supported bowser, initializing...");
-		this.data.compatibility=true;
+phoenix.core.init = function () {
+	if (
+		function () {
+			if (bowser.msie && bowser.version <= 8) {
+			  	return false;
+			}
+			else{
+				return true;
+			}
+		}
+	) 
+	{
+		console.log("[phoenix][core] supported bowser, initializing...");
+		phoenix.data.core.compatibility=true;
+		if(typeof jQuery !== "undefined") phoenix.data.core.modulesList.jquery=true;
 	}
 	else{
 		console.error("[phoenix-init] not supported bowser");
 	};
 };
 
-phoenix.init();
+phoenix.core.init();
--- demo/js/phoenix/modules/variables.js
+++ demo/js/phoenix/modules/variables.js
@@ -5,15 +5,17 @@
  * ex. js/app/variables-app.js , but you can use another file too,
  * just reamember to add it between variables.js and rest of phoenix modules.
  */
-if (phoenix.data.compatibility) {
-	console.log("[phoenix-variables-init] module initializing...");
-	phoenix.data.modulesList.variables=true;
+if (phoenix.core.checkComp()) {
+	phoenix.core.log("start_module_loading","variables")
+	phoenix.data.variables= new Object();
+	var data=phoenix.data.variables;
 	/**
 	 * You can edit all this variables. Possible values are in comments below,
 	 * but before change anythink you should read at list connected to it part of documentation. 
 	 */
-	
+	phoenix.core.addModule("variables");
+	phoenix.core.log("finish_module_loading","variables")
 }
 else{
-	console.error("[phoenix-variables-init] incompatibility detected, module will NOT be initialized");
+	phoenix.core.log("could_not_load_module",{id:"variables",reason:"comp"})
 };
